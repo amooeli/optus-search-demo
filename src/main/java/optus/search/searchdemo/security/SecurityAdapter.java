1be872a6fa1171c9012aa644769d25f07ef9fa75
ali <saaiydnia@hotmail.com>
2018-08-05 21:41:00 +1000

    Cleanup...

--- src/main/java/optus/search/searchdemo/security/SecurityAdapter.java
+++ src/main/java/optus/search/searchdemo/security/SecurityAdapter.java
@@ -1,11 +1,7 @@
-/**
- *
- */
 package optus.search.searchdemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +9,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
